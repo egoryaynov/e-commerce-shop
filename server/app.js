f3077880c5016038f6cd0b1d7913dbf4f5e3d55a
egoryaynov <egor.goryaynov02@gmail.com>
2021-03-15 22:07:08 +0400

    Drop files from .gitignore

--- server/app.js
+++ server/app.js
@@ -1,4 +1,8 @@
+require('dotenv').config({path: './config.env'})
 const express = require('express')
 const app = express()
 
-app.listen(5000)
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
