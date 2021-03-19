07c96f0b4789a16b70e905a8c0ffa53f5186f18f
egoryaynov <egor.goryaynov02@gmail.com>
2021-03-20 00:55:37 +0400

    set address (POST) /address endpoint

--- server/controllers/address.controller.js
+++ server/controllers/address.controller.js
@@ -6,8 +6,18 @@ exports.setAddress = async (req, res, next) => {
     const full = `${address.street}, ${address.houseNumber}, ${address.apartmentNumber}, ${address.city}, ${address.country}, ${address.postcode}`
 
     try {
+        const user = req.user
+
         await Address.create({
-            user: req.user._id, ...address, full
+            user: user._id, ...address, full
+        }, async (err, newAddress) => {
+            if (err) {
+                next(new ErrorResponse('Incorrect request body', 400))
+            }
+
+            user.addresses.push(newAddress._id)
+
+            await user.save()
         })
 
         res.status(200).json({
