ed5db407941bcb9e5f962729c709203b469fd644
Egor Goryaynov <egor.goryaynov02@gmail.com>
2021-07-09 02:29:35 +0400

    order handle in delivery service

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectDB = async () => {
+const connectDB = async (serverName = 'Server') => {
     await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
@@ -10,7 +10,7 @@ const connectDB = async () => {
         if (err) {
             throw new Error('Error on connect DB')
         } else {
-            console.log('[Server]: MongoDB connected')
+            console.log(`[${serverName}]: MongoDB connected`)
         }
     })
 }
--- server/services/deliveryService/app.js
+++ server/services/deliveryService/app.js
@@ -1,24 +1,43 @@
 require('dotenv').config({path: '././config.env'})
+const connectDB = require('../../config/db')
+
+const Order = require('../../models/Order')
 
 const io = require("socket.io-client")
 
-// сейвим заказы в json и при статусе доставлено удаляем из него заказ
-// при запуске этого сервиса проверяем есть ли данные в json, если есть - обработать их
+const generateTimeToChangeStatus = () => {
+    return Math.round(Math.random() * 5 + 1) * 60 * 1000
+}
 
-// time to stage Math.round(Math.random() * 5 + 1)
+connectDB('DeliveryService').then(() => {
+    const socket = io(process.env.DELIVERY_WEBSOCKET_URL, {
+        reconnection: true
+    })
 
-// Connect to websocket on server
-const socket = io(process.env.DELIVERY_WEBSOCKET_URL, {
-    reconnection: true
-})
+    // stages: 1 - paid, 2 - picked, 3 - delivered (last stage in delivery service), 4 - received
 
-socket.on('connect', function () {
-    // socket.on('hello', function (data) {
-    //     console.log('message from the server:', data);
-    //     socket.emit('serverEvent', "thanks server! for sending '" + data + "'");
-    // });
+    const stages = ['paid', 'picked', 'delivered']
 
-    socket.on('new_order', (data) => {
-        console.log('NEW ORDER IN DELIVERY', data)
+    socket.on('connect', async function () {
+        const orders = await Order.find({finished: false})
+
+        orders.forEach(order => {
+            const interval = setInterval(() => {
+                const newStatusName = stages[stages.indexOf(order.status) + 1]
+
+                socket.emit('change_status', {orderId: order._id, status: newStatusName})
+
+                if (newStatusName === 'delivered') clearInterval(interval)
+            }, generateTimeToChangeStatus())
+        })
+
+        // socket.on('hello', function (data) {
+        //     console.log('message from the server:', data);
+        //     socket.emit('serverEvent', "thanks server! for sending '" + data + "'");
+        // });
+
+        socket.on('new_order', (data) => {
+            console.log('NEW ORDER IN DELIVERY', data)
+        })
     })
 })
